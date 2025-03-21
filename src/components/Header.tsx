0fa1b13f8f6a84c1acc0148992956865cd6c30b2
gomezdn <gomezdn@outlook.com>
2025-03-21 20:50:55 -0300

    feat: add a sticky Header component to root layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import logo from '@public/svg/lanacion-logo.svg'
+import hamburgerIcon from '@public/svg/hamburger-menu-icon.svg'
+import magnifyingIcon from '@public/svg/magnifying-glass-icon.svg'
+import favouritesIcon from '@public/svg/favourites-icon.svg'
+import notificationsIcon from '@public/svg/notifications-icon.svg'
+import smileIcon from '@public/svg/smile-icon.svg'
+
+export default function Header() {
+    return (
+        <header className='flex justify-between items-center w-full bg-white bg-opacity-[0.08] px-10 sticky top-0'>
+            <div className="flex items-center">
+                <button>
+                    <Image alt='menu icon' src={hamburgerIcon} height={30} width={32}/>
+                </button>
+                <button>
+                    <Image alt='logo de La Nacion' src={logo} height={100} width={100}/>
+                </button>
+            </div>
+
+            <div className='flex gap-3 items-center w-max'>
+                <form className='flex gap-3 w-100'>
+                    <div>
+                        <input className='w-[350px] p-3 pl-6 bg-white bg-opacity-0 rounded-l-full border-y-[1px] border-l-[1px] outline-none' name='shop' placeholder='Busca un comercio...'/>
+                        <input className='w-[350px] p-3 pl-6 bg-white bg-opacity-0 rounded-r-full border-y-[1px] border-[1px] outline-none' name='location' placeholder='Ingresa una ubicación...'/>
+                    </div>
+                    <button className='bg-blue-600 rounded-full p-4' type='submit'>
+                        <Image alt='submit search button' src={magnifyingIcon} height={17} width={17}/>
+                    </button>
+                </form>
+            </div>
+
+            <div className='flex gap-4 items-center'>
+                <button>
+                    <Image alt='notifications button' src={notificationsIcon} height={28} width={28}/>
+                </button>
+                <button>
+                    <Image alt='favourites button' src={favouritesIcon} height={28} width={28}/>
+                </button>
+                <button>
+                    <Image alt='smile icon' src={smileIcon} height={28} width={28}/>
+                </button>
+            </div>
+        </header>
+    )
+}
